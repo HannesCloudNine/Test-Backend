b94fbe9bfd12c9bdf306410bf72b45cf33ffb22b
Hannes Westberg <hannes.westberg@cloudnine.se>
2023-01-04 17:18:24 +0100

    Test breaking change

--- TechTestBackend/SpotifyHelper.cs
+++ TechTestBackend/SpotifyHelper.cs
@@ -8,6 +8,7 @@ public static class SpotifyHelper
 {
     public static Soptifysong[] GetTracks(string name)
     {
+        return null;
         var client = new HttpClient();
         var c_id = "996d0037680544c987287a9b0470fdbb";
         var c_s = "5a3c92099a324b8f9e45d77e919fec13";
